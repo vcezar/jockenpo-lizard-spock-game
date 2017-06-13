62a2e714b61dd9f5037adfaf84ed93cb5322bb19
Mariana Oliveira <mariana.oliveira@uniriotec.br>
2017-06-12 22:48:51 -0300

    Main variables

---
+++ baby-code.py
@@ -0,0 +1,12 @@
+#variables declaration of the game; it will be used positive int numbers
+rock = 0
+paper = 1
+scissor = 2
+lizard = 3
+spock = 4
+
+#variable for the gamer
+gamer = input('Make your move!')
+
+#random variable for the computer
+computer = random.randint(0, 4)
